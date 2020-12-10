51b4bc27dc44a34dc2ee7da6f2083ab80f13d600
EmelyanovKirill <kivanov2302@yandex.ru>
2020-12-10 20:49:07 +0300

    ДЗ

---
+++ Homework1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // объявление переменных
+            string name;
+            DateTime date;
+            // Сообщение
+            Console.WriteLine("Введите имя:");
+            // присвоение значений переменным
+            name = Console.ReadLine();
+            date = DateTime.Now;
+            //Вывод результата
+            Console.WriteLine($"Привет, {name}, сегодня {date}");
+            Console.ReadKey();
+        }
+    }
+}
