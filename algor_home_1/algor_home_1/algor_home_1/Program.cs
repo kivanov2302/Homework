22e4d5987c8f1a9fdb2d17e1c08d6c588bb750d8
EmelyanovKirill <kivanov2302@yandex.ru>
2021-01-17 15:51:33 +0300

    Алгаритмы ДЗ 1

---
+++ algor_home_1/algor_home_1/algor_home_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algor_home_1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = 5;
+            Console.WriteLine(f(n));
+            n = 4;
+            Console.WriteLine(f(n));
+        }
+        static bool f(int number)
+        {
+            int d = 0;
+            int i = 2;
+            while (i < number)
+            {
+                if (number % i == 0)
+                    d++;
+                i++;
+            }
+
+            return d == 0;
+        }
+    }
+}
