2725d135a2d531e6078043bed01853a4173069a7
EmelyanovKirill <kivanov2302@yandex.ru>
2021-01-17 15:51:33 +0300

    Алгоритмы ДЗ 1

---
+++ algor_home_1/algor_home_1_2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algor_home_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 12, 2, 32, 15 };
+            StrangeSum(arr);
+        }
+        public static int StrangeSum(int[] inputArray)
+        {
+            int sum = 0;
+            for (int i = 0; i < inputArray.Length; i++)             //O(N)
+            {
+                for (int j = 0; j < inputArray.Length; j++)         //O(N)
+                {
+                    for (int k = 0; k < inputArray.Length; k++)     //O(N)
+                    {
+                        int y = 0;                                  //O(1N)
+
+                        if (j != 0)                                 //O(1N)
+                        {
+                            y = k / j;                              //O(1N)
+                        }
+
+                        sum += inputArray[i] + i + k + j + y;       //O(1N)
+                    }
+                }
+            }
+                                                                    //O(4N^3)
+            return sum;
+        }
+    }
+ }
---
+++ algor_home_1/algor_home_1_3/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algor_home_1_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = 0;
+            Console.WriteLine(f_rec(n) + "/"+ f_loop(n));
+            n = 1;
+            Console.WriteLine(f_rec(n) + "/" + f_loop(n));
+            n = 2;
+            Console.WriteLine(f_rec(n) + "/" + f_loop(n));
+            n = 3;
+            Console.WriteLine(f_rec(n) + "/" + f_loop(n));
+            n = 4;
+            Console.WriteLine(f_rec(n) + "/" + f_loop(n));
+            n = 10;
+            Console.WriteLine(f_rec(n) + "/" + f_loop(n));
+            n = 20;
+            Console.WriteLine(f_rec(n) + "/" + f_loop(n));
+        }
+
+        static int f_rec(int n) // O(2N^2)
+        {
+            int result = 0;
+            if (n == 0)
+                result = 0;
+            else if (n == 1)
+                result = 1;
+            else
+                result = f_rec(n - 2) + f_rec(n - 1);
+            return result;
+        }
+
+        static int f_loop(int n) // O(3N)
+        {
+            int f_0 = 0;
+            int f_1 = 1;
+            int result = 0;
+            if (n == 1)
+                result = f_1;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                result = f_0 + f_1;
+                f_0 = f_1;
+                f_1 = result;
+            }
+
+            return result;
+        }
+    }
+}
