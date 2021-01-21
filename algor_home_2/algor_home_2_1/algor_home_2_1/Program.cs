c454f46894551be2b29783610d2034c5636f0bc3
EmelyanovKirill <kivanov2302@yandex.ru>
2021-01-22 02:10:33 +0300

    Алгаритмы ДЗ 2

---
+++ algor_home_2/algor_home_2_1/algor_home_2_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algor_home_2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nodelist = new LinkedList();
+
+            nodelist.AddNode(1);
+            nodelist.AddNode(2);
+            nodelist.AddNode(3);
+
+            var node = new Node(1);
+            nodelist.AddNodeAfter(node, 9);
+
+            node = new Node(2);
+            nodelist.AddNodeAfter(node, 8);
+
+            node = new Node(3);
+            nodelist.AddNodeAfter(node, 7);
+
+            node = nodelist.FindNode(3);
+
+            nodelist.RemoveNode(2);
+
+            node = new Node(8);
+            nodelist.RemoveNode(node);
+        }
+    }
+}   
---
+++ algor_home_2/algor_home_2_2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algor_home_2_2
+{
+    class Program
+    {
+        static int[] a = new int[20];
+        static int counter;
+        static void Main(string[] args)
+        {
+            arrGen();
+            rez(a); 
+            Console.ReadKey();
+        }
+        static void rez(int[] a)
+        {
+            Console.Write("Ввести элемент поиска: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int k = binPoisk(b);    // O(log n)
+            Console.WriteLine("Бинарный поиск:");
+            if (k > -1)
+                Console.WriteLine("Номер элемента = {0}, число сравнений внутри цикла = {1}", k, counter);
+            else
+                Console.WriteLine("нет заданного элемента, число сравнений внутри цикла = {0}", counter);
+        }
+        static void arrGen()
+        {
+            Random ran = new Random();
+            a[0] = ran.Next(1, 6);
+            Console.Write("  {0}", a[0]);
+            for (int i = 1; i < a.Length; i++)
+            {
+                a[i] = a[i - 1] + ran.Next(1, 6);
+                Console.Write("  {0}", a[i]);
+            }
+            Console.WriteLine();
+        }
+        static int binPoisk(int b)
+        {
+            int k;
+            int L = 0;
+            int R = a.Length - 1;
+            k = (R + L) / 2;
+            while (L < R - 1)
+            {
+                counter++;
+                k = (R + L) / 2;
+                if (a[k] == b)
+                    return k;
+                counter++;
+                if (a[k] < b)
+                    L = k;
+                else
+                    R = k;
+            }
+            if (a[k] != b)
+            {
+                if (a[L] == b)
+                    k = L;
+                else
+                {
+                    if (a[R] == b)
+                        k = R;
+                    else
+                        k = -1;
+                };
+            }
+            return k;
+        }
+    }
+}
